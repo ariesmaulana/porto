0105e5c7ab963ecfc5db9ef0925fdd22a50e28ff
ariesm <ariesmpernama@gmail.com>
2025-11-23 17:19:01 +0700

    url asset

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from project import views
 
 urlpatterns = [
@@ -9,7 +9,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("projects/", views.project_list, name="projects"),
     path("projects/<int:pk>/", views.project_detail, name="project_detail"),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
